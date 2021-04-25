a8b4cd8d087f18bdebd5e1fd0f09fb3052075112
wildbook7 <wildbook7@gmail.com>
2021-04-25 21:26:23 +0900

    ファイル削除前

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App(props) {
+  const subject = props.subject;
+  console.log();
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello, {subject}!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
