e4c049f4a58cb9420f6320b8f8724f4fd5f32fef
wildbook7 <wildbook7@gmail.com>
2021-04-26 06:40:30 +0900

    key prop 使用の手前まで

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App subject="Clarice" />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false }
+];
+
+ReactDOM.render(<App tasks={DATA} />, document.getElementById("root"));
